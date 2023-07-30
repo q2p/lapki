dc214a41f027784000b296b1e788ebf6edab4b74
egorkasprigorca <71177475+EgorBolotow@users.noreply.github.com>
2023-07-30 16:13:23 +0300

    config

--- tauri_paper/src-tauri/src/main.rs
+++ tauri_paper/src-tauri/src/main.rs
@@ -1,15 +1,79 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::Path;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Write};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Config {
+  walls: Vec<Wall>,
+  radio_points: Vec<RadioPoint>,
+  radio_zones: Vec<RadioZone>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Wall {
+  a: Point2d,
+  b: Point2d,
+  damping: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RadioPoint {
+  pos: Point2d,
+  power: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RadioZone {
+  points: Vec<Point2d>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point2d {
+  x: i32,
+  y: i32,
+}
+
+fn read_config<P: AsRef<Path>>(path: P) -> Config {
+  let file = File::open(path).unwrap();
+  let reader = BufReader::new(file);
+
+  let c: Config = serde_json::from_reader(reader).unwrap();
+  c
+}
+
+fn write_config<P: AsRef<Path>>(config: Config, path: P){
+  let file = File::create(path).unwrap();
+  let mut writer = BufWriter::new(file);
+  let json = serde_json::to_string(&config).unwrap();
+  writer.write_all(json.as_bytes()).expect("unable to write");
+}
+
+#[tauri::command]
+fn get_config() -> Config {
+  read_config("config.json")
+}
+
 fn main() {
+  // for a in std::fs::read_dir(".").unwrap() {
+  //   println!("{}", a.unwrap().path().to_str().unwrap())
+  // }
+  // let mut config = read_config("config.json");
+  // config.walls[0].a.x = 6969;
+  // write_config(config, "config.json");
+
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![get_config])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- tauri_paper/src/main.ts
+++ tauri_paper/src/main.ts
@@ -1,22 +1,46 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
+interface Config {
+  walls: Wall[];
+  radio_points: RadioPoint[];
+  radio_zones: RadioZone[];
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+interface Wall {
+  a: Point2d;
+  b: Point2d;
+  damping: number;
+}
+
+interface RadioPoint {
+  pos: Point2d;
+  power: number;
+}
+
+interface RadioZone {
+  points: Point2d[];
+}
+
+interface Point2d {
+  x: number;
+  y: number;
+}
+
+async function get_config(): Promise<Config>{
+  return invoke("get_config")
+    .then((cfg: Config) => {return cfg} );
+}
+
+let config: Config = await get_config();
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D | null;
+
+canvas = document.getElementById('canvas') as HTMLCanvasElement;
+context = canvas.getContext("2d");
+
+context?.beginPath();
+for (let i = 0; i < config.walls.length; i++) {
+  context?.moveTo(config.walls[i].a.x, config.walls[i].a.y);
+  context?.lineTo(config.walls[i].b.x, config.walls[i].b.y);
+}
+context?.stroke();
