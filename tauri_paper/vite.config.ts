e4b8e2bcd523f26745935c702f06f380318f6ee3
q2p <q2p@users.noreply.github.com>
2024-03-05 13:51:04 +0700

    Auto fix ESLint

--- tauri_paper/vite.config.ts
+++ tauri_paper/vite.config.ts
@@ -12,7 +12,9 @@ export default defineConfig(async () => ({
     port: 1420,
     strictPort: true,
   },
-  plugins: [eslint()],
+  plugins: [eslint({
+    fix: true,
+  })],
   // 3. to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
